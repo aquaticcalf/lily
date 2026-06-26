03bdafb1dd7811202d0d248ffc80baef9c2ae4f0
sam <aquaticcalf@proton.me>
2026-06-27 03:40:22 +0530

    remove kotlin

--- android/ui/src/color/base.kt
+++
@@ -1,9 +0,0 @@
-package ui.color
-
-import androidx.compose.ui.graphics.Color
-
-object BaseColors {
-    val white = Color(0xFFFFFFFF)
-    val black = Color(0xFF000000)
-    val transparent = Color(0x00000000)
-}
