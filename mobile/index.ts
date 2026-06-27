83f74e41056b5a6502fd91c4aa4d54f08357d9a0
sam <aquaticcalf@proton.me>
2026-06-27 07:33:48 +0530

    delete mobile

--- mobile/index.ts
+++
@@ -1,7 +0,0 @@
-import { registerRootComponent } from "expo"
-
-function App() {
-    return null
-}
-
-registerRootComponent(App)
--- mobile/theme/primitives/index.ts
+++
@@ -1,7 +0,0 @@
-export { Box } from "./Box"
-export type { ThemedViewProps as BoxProps } from "./Box"
-export { ThemedText } from "./Text"
-export type { ThemedTextProps as TextProps } from "./Text"
-export { HStack, VStack } from "./Stack"
-export type { HStackProps, VStackProps } from "./Stack"
-export { ThemedScrollView, ThemedPressable, ThemedTextInput } from "./inputs"
